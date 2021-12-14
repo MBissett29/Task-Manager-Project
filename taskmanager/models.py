5e88fdbc56ad0409c1a8cd862d60ebbb21be732f
MBissett29 <mbissett@hotmail.com>
2021-12-14 14:20:12 +0000

    Add a categories page using GET POST python methods

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    categroy_name = db.Column(db.String(25), unique=True, nullable=False)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="categroy", cascade="all, delete", lazy=True)
 
     def __repr__(self):
